ebf6423bbe18ac0f1844ce089eaac2e208ac1e50
Zaid Mohmand <zaidmohmand@hotmail.com>
2025-01-16 17:48:33 -0500

    integrate scripts 2

--- geq.js
+++ geq.js
@@ -1,6 +1,8 @@
 (() => {
-    const geq = (window.geq = window.geq || []);
+    const geq = (self.geq = self.geq || []);
+  
     if (geq.initialize) return;
+  
     if (geq.invoked) {
       console.error("GE snippet included twice.");
       return;
@@ -33,26 +35,9 @@
       geq[key] = geq.factory(key);
     }
   
-    geq.load = (key) => {
-      // Dynamically load external script
-      fetch(
-        location.href.includes("vge=true")
-          ? `https://s3-us-west-2.amazonaws.com/jsstore/a/${key}/ge.js?v=${Math.random()}`
-          : `https://s3-us-west-2.amazonaws.com/jsstore/a/${key}/ge.js`
-      )
-        .then((response) => response.text())
-        .then((scriptText) => {
-          eval(scriptText);
-          console.log("Script loaded successfully.");
-        })
-        .catch((err) => console.error("Error loading script:", err));
-    };
-  
     geq.SNIPPET_VERSION = "1.6.1";
-    geq.load("X2JH31E");
   
-    // Trigger the page method
-    geq.page = () => console.log("geq.page() called");
+    // Trigger the `page` method
     geq.page();
   })();
   
